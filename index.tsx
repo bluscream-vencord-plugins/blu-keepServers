0aea9eeb492da7f3d27e5152c9a97cf32ecca410
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 02:53:19 +0100

    Refactor: Adhered to Gold Standard conventions

--- index.tsx
+++ index.tsx
@@ -21,18 +21,8 @@ import {
 import { getNative } from "./nativeUtils.js";
 // endregion Imports
 
-// region PluginInfo
-export const pluginInfo = {
-    id: "keepServers",
-    name: "KeepServers",
-    description: "Keeps track of servers you've joined and allows you to rejoin them later",
-    color: "#7289da",
-    authors: [
-        { name: "Bluscream", id: 467777925790564352n },
-        { name: "Assistant", id: 0n }
-    ],
-};
-// endregion PluginInfo
+import { pluginInfo } from "./info";
+export { pluginInfo };
 
 // region Variables
 const logger = new Logger(pluginInfo.id, pluginInfo.color);
---
+++ info.ts
@@ -0,0 +1,10 @@
+export const pluginInfo = {
+    id: "keepServers",
+    name: "KeepServers",
+    description: "Keeps track of servers you've joined and allows you to rejoin them later",
+    color: "#7289da",
+    authors: [
+        { name: "Bluscream", id: 467777925790564352n },
+        { name: "Assistant", id: 0n }
+    ],
+};
